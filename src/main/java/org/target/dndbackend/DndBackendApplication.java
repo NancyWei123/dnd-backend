e21e6a5e1753aac26b30fffc51999b023b1c8134
Nancy Wei <nancywei20030416@gmail.com>
2026-04-19 11:40:51 +1000

    springboot init

---
+++ src/main/java/org/target/dndbackend/DndBackendApplication.java
@@ -0,0 +1,13 @@
+package org.target.dndbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/target/dndbackend/DndBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.target.dndbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
